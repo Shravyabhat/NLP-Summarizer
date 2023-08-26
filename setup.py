6699c9ccd3c9205450a0845bfaa8bf7f2c3f6768
Shravyabhat <shravya.kanalli@gmail.com>
2023-08-27 01:34:11 +0530

    x

--- setup.py
+++ setup.py
@@ -12,5 +12,17 @@ AUTHOR_EMAIL="shravya.kanalli@gmail.com"
 
 
 setuptools.setup(
-    
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Package for NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",   
+    },
+    package_dir={"","src"},
+    packages=setuptools.find_packages(where="src")
 )
