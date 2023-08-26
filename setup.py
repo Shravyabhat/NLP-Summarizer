79253f4b91000f9216080622307d7bc38dc4985e
Shravyabhat <shravya.kanalli@gmail.com>
2023-08-27 00:44:55 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools # This setup.py is required to make a local package 
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description=f.read()
+
+__version__="0.0.0"
+
+REPO_NAME="NLP-Summarizer"
+AUTHOR_USER_NAME="Shravyabhat"
+SRC_REPO="Summarizer"
+AUTHOR_EMAIL="shravya.kanalli@gmail.com"
+
+
+setuptools.setup(
+    
+)
